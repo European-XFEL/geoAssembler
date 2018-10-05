dd69126b3a7a4b7257006985898f54a0e72df33b
Martin Bergemann <martin.bergemann@xfel.eu>
2018-10-05 17:47:56 +0200

    Circle centers are aligned according to weighted average

--- Assembler.py
+++ Assembler.py
@@ -9,7 +9,7 @@ import pandas as pd
 import sys
 import re
 from itertools import product
-
+from collections import namedtuple
 
 def Test():
     '''
@@ -98,6 +98,9 @@ class Assemble(object):
                 # Read the CrystFel geometry information
                 self.__from_crystfel(df)
 
+        ## Which method is used for fitting (default None)
+        self.fit_method = None
+
     @staticmethod
     def _make_df():
         # This method creates a standard geometry, which can be overwritten later
@@ -117,12 +120,11 @@ class Assemble(object):
                                         'SPB_DET_AGIPD1M-1/DET/13CH0:xtdf',
                                         'SPB_DET_AGIPD1M-1/DET/14CH0:xtdf',
                                         'SPB_DET_AGIPD1M-1/DET/15CH0:xtdf'],
-                             'Xoffset': [0,  158,  316,  474,  662,
-                                         820,  978, 1136,  712,  870, 1028,
-                                         1186,   50,  208,  366,  524],
-                             'Yoffset': [612, 612, 612, 612, 612, 612, 612,
-                                         612,   0,   0,   0,   0,   0,
-                                         0,   0,   0],
+                            'Yoffset' : [542, 542, 542, 542, 542, 542, 542, 542,
+                                           0, 0, 0, 0, 0, 0, 0, 0],
+                            'Xoffset' : [   0,  158,  316,  474,  632,  790,
+                                          948, 1106, 632,  790,  948,  1106, 
+                                          0, 158, 316, 474],
                              'FlipX': [True,  True,  True,  True,  True,
                                        True,  True,  True, False,  False,
                                        False, False, False, False, False,
@@ -227,7 +229,7 @@ class Assemble(object):
         Returns:
            Nd-array : numpy ND array for the image.data of the detector
         '''
-
+        old_df = self.df
         Yoffset=[542, 542, 542, 542, 542, 542, 542, 542, 0, 0, 0, 0, 0, 0, 0, 0]
         Xoffset=[   0,  158,  316,  474,  632,  790,  948, 1106, 632,  790,  948,
              1106, 0, 158, 316, 474]
@@ -274,6 +276,8 @@ class Assemble(object):
             else:
                 arr[...,iy:iy+shapey, ix:ix+shapex] = d.T
 
+        self.__df = old_df
+        self.df = self.__set_df
         return arr#.reshape(*newshape)
 
     def __from_crystfel(self, filename):
@@ -335,3 +339,165 @@ class Assemble(object):
         for i in range(8, 16):
             new_data.at[i, 'Xoffset'] += 128
         return new_data
+
+
+    def get_geometry(self, data, vmax=5000, vmin=-1000):
+        '''Method that creates a new detector geometry according to a given fit
+            method
+            Parameters:
+                data (dict-object): dectector data stored in modules that is 
+                typically returned by karabo-data
+            Keywords:
+                fit_method (str) : the mothod that is used for fitting 
+                                   (default : circle)
+                                   at the moment there are the following options:
+                                     - circle : for fitting circles to the data
+                vmax (int) :  maximum value to be displayed when plotting the data
+                vmin (int) :  minimum value to be displayed when plotting the data
+        '''
+        ## Start with the Gui
+        from Gui import PanelView
+
+        ## 1.0 Let the user define the points for alignment
+        View = PanelView(self.stack(data), vmax=vmax, vmin=vmin)
+
+        self.fit_method = View.fit_method
+        if self.fit_method.lower() == 'circle':
+            return self.__shift_circle(View.points)
+
+        else:
+            raise NotImplementedError('At the moment only circle, fits are available')
+
+
+    def __shift_circle(self, points):
+        '''Method to shift detector Quadrants in order to make circles
+
+            Parameters:
+                points (namedtuple): The points in each quadrant that will make
+                                     the circle
+                geometry (pandas dataframe): A pandas dataframe containing the 
+                                             detector information
+        '''
+        tpoints = namedtuple('Points','x y')
+        from Fit import fit_circle
+        center = []
+        radius =[]
+        residu = []
+        for quad, pnt in points.items():
+            c_x, c_y, r, res, ncalls = fit_circle(pnt)
+            center.append(np.array([c_x, c_y]))
+            radius.append(r)
+            residu.append(res)
+        residu = np.array(residu)
+        radius = np.array(radius)
+        center = np.array(center)
+        c_mean = np.average(center, weights=1-residu/residu.sum(), axis=0)
+        #c_mean = np.average(center, axis=0)
+        shift = c_mean - center
+        X = self.__df.Xoffset.values
+        Y = self.__df.Yoffset.values
+        ## That should be the shape of current array (dy, dx)
+
+        x_comb = []
+        y_comb = []
+        for i in range(len(shift)):
+            quad=i+1
+            idx=np.array(self.__df.loc[self.__df.Quadrant == quad].index)
+            X[idx] += int(shift[i,1].astype('i'))
+            Y[idx] += int(shift[i,0].astype('i'))
+            for ii in range(len(points[quad].x)):
+                x_comb.append(points[quad].x[ii] + int(shift[i,0]))
+                y_comb.append(points[quad].y[ii] + int(shift[i,1]))
+
+
+        dx2 = abs(self.__df.loc[self.__df.Quadrant == 2].Yoffset.values[0])
+        dx1 = abs(self.__df.loc[self.__df.Quadrant == 4].Yoffset.values[0])
+        #dy1 = abs(self.__df.loc[self.__df.Quadrant == 1].Xoffset.values[0])
+        #dy2 = abs(self.__df.loc[self.__df.Quadrant == 2].Xoffset.values[0])
+        dx = dx1 - dx2
+        #dy = dy1 - dy2
+        #I don't understand why this works but is does
+        new_points = tpoints(x=np.array(x_comb)+dx/2, y=np.array(y_comb))
+
+        nc_x, nc_y, self.radius, nres, ncalls =fit_circle(new_points)
+        X = np.array(X)
+        Y = np.array(Y)
+
+        if Y.min() < 0:
+            Y += np.fabs(Y.min()).astype(Y.dtype)
+        if X.min() < 0:
+            X += np.fabs(X.min()).astype(X.dtype)
+
+        self.__df['Xoffset']=X
+        self.__df['Yoffset']=Y
+
+        self.df = self.__set_df
+        self.radius = radius.mean()
+        self.center = np.array([nc_x,nc_y])
+        self.center[0] #+= dx
+        self.center[1] #+= dy
+        self.points = new_points
+
+
+    def plot_data(self, data, *args, vmin=-1000, vmax=5000, **kwargs):
+        '''Visualize assembled data from
+            Parameters:
+                data : dictionary that contains the detector image
+
+            Keywords:
+                vmin : minimum value of the plot to be displayed
+                vmax : maximum value of the plot to be displayed
+                kwargs : additional arguments for the plotting
+        '''
+
+        img = self.apply_geo(data)
+        from matplotlib import pyplot as plt
+        from matplotlib import cm
+        cmap = cm.Greys_r
+        cmap.set_bad('k')
+        cmap.set_over('k')
+        cmap.set_under('k')
+        img = np.ma.masked_outside(np.ma.masked_invalid(img), vmin, vmax)
+        plt.imshow(img, vmin=vmin, vmax=vmax, cmap=cmap)
+
+        if self.fit_method == 'circle':
+            ## Plot the result
+
+            theta_fit = np.linspace(-np.pi, np.pi, 180)
+            x_fit2 = self.center[0] + self.radius*np.cos(theta_fit)
+            y_fit2 = self.center[1] + self.radius*np.sin(theta_fit)
+
+            plt.plot(x_fit2, y_fit2, linestyle='--', lw=1, label='Fit', color='r')
+            plt.scatter([self.center[0]], self.center[1], s=20, color='r')
+            plt.scatter(self.points.x, self.points.y, s=15, color='b')
+
+        plt.legend(loc=0)
+        plt.show()
+
+
+
+
+def get_testdata():
+    '''Method to get some test-data'''
+    import os
+    array = np.load(os.path.join(os.path.dirname(__file__), 'image.npz'))
+    ## Create some mock test data as it would be comming from karabo-data
+    data = Test()
+    for n,k in enumerate(data.keys()):
+         data[k]['image.data'] = array['image.%02i'%n]
+
+    return data
+
+
+if __name__ == '__main__':
+
+    vmin, vmax = -1000, 5000
+    ## Get some mock data for testing
+    data = get_testdata()
+    
+    A = Assemble()
+    ## do the shifting with some test-data that is saved in this directory
+    A.get_geometry(data)
+    A.plot_data(data, vmin=vmin, vmax=vmax)
+
+    
