a167303aeb0424336255bbe169093c96fb935982
Martin Bergemann <martin.bergemann@monash.edu>
2018-12-20 16:41:14 +0100

    Add Qt5 as requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Setup script for intalling the geoAssembler."""
 import os.path as osp
 import re
 from setuptools import setup, find_packages
@@ -6,14 +7,17 @@ import sys
 
 
 def get_script_path():
+    """Return the directory path of this file."""
     return osp.dirname(osp.realpath(sys.argv[0]))
 
 
 def read(*parts):
+    """Read the content of a file."""
     return open(osp.join(get_script_path(), *parts)).read()
 
 
 def find_version(*parts):
+    """Get the version number of the lib."""
     vers_file = read(*parts)
     match = re.search(r'^__version__ = "(\d+\.\d+\.\d+)"', vers_file, re.M)
     if match is not None:
@@ -27,7 +31,7 @@ setup(name="geoAssembler",
       author_email="cas-support@xfel.eu",
       maintainer="Martin Bergemann",
       url="https://git.xfel.eu/gitlab/dataAnalysis/geoAssembler",
-      description="Tool to calibrate Detector Geometry based on Power diffraction pattern",
+      description="Tool to Calibrate Detector Geometry Based on Power Diffraction Pattern",
       long_description=read("README.md"),
       license="BSD-3-Clause",
       packages=find_packages(),
@@ -36,6 +40,7 @@ setup(name="geoAssembler",
           'matplotlib',
           'numpy',
           'pyqtgraph',
+          'PyQt5'
       ],
       extras_require={
           'docs': [
@@ -48,7 +53,7 @@ setup(name="geoAssembler",
               'testpath',
           ]
       },
-      python_requires='>=3.4',
+      python_requires='>=3',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
@@ -57,9 +62,6 @@ setup(name="geoAssembler",
           'License :: OSI Approved :: BSD License',
           'Operating System :: POSIX :: Linux',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
           'Topic :: Scientific/Engineering :: Information Analysis',
           'Topic :: Scientific/Engineering :: Physics',
       ]
