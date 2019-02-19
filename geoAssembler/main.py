6f721ba64f38d945fa91f39ab68a0806bd64833c
Martin Bergemann <martin.bergemann@xfel.eu>
2019-02-19 11:46:16 +0100

    Do not check for input parameters

--- geoAssembler/main.py
+++ geoAssembler/main.py
@@ -4,12 +4,8 @@
 from argparse import ArgumentParser
 import logging
 import os
-import re
-import readline
 
-from ipykernel import kernelspec
 from pyqtgraph import QtGui
-
 from . import CalibrateQt
 
 logging.basicConfig(level=logging.INFO)
@@ -27,97 +23,29 @@ adu_per_eV = 0.0075  ; no idea
 clen = {clen}  ; Camera length, aka detector distance
 photon_energy = {energy} ;"""
 
-RE_SPACE = re.compile('.*\s+$', re.M)
-
-NB_MESSAGE = """
-
-Notebook has been created. You can use it by loading the file
+NB_MESSAGE = """Notebook has been created. You can use it by loading the file
 {nb_path} either by using JupyterHub on desy:
  https://max-jhub.desy.de
 or by starting a jupyter server:
- jupyter-notebook --port PORT_NUBER* --no-browser &
-and follow the displayed instructions. For more information see https://bit.ly/2Gm97c0
+ jupyter-notebook --port PORT_NUMBER* --no-browser &
+and follow the displayed instructions. For more information see:
+
+https://in.xfel.eu/readthedocs/docs/data-analysis-user-documentation/en/latest/computing.html#can-i-execute-a-jupyter-notebook-on-the-maxwell-cluster-and-connect-it-to-the-webbrowser-of-my-desktop-laptop
 
 
 Note 1: the PORT_NUMBER should be a number of >= 1024 like 8432
 """
 
-
-class Completer:
-    """Provide tab completion directories and files for call of input."""
-
-    def _listdir(self, root):
-        """List directory 'root' appending the path separator to subdirs."""
-        res = []
-        for name in os.listdir(root):
-            path = os.path.join(root, name)
-            if os.path.isdir(path):
-                name += os.sep
-            res.append(name)
-        return res
-
-    def complete(self, text: str, state: int):
-        """Define the complet method for readline."""
-        try:
-            path = self.complete_path(text)
-        except Exception:
-            return None
-        if state >= len(path):
-            return None
-        return path[state]
-
-    def complete_path(self, path):
-        """Perform completion of filesystem path."""
-        if not path:
-            return self._listdir('.')
-        dirname, rest = os.path.split(os.path.expanduser(path))
-        tmp = dirname if dirname else '.'
-        res = [os.path.join(dirname, p)
-               for p in self._listdir(tmp) if p.startswith(rest)]
-        # more than one match, or single match which does not exist (typo)
-        if len(res) > 1 or os.path.exists(res[0]):
-            return res
-        # resolved to a single directory, so return list of files below it
-        if os.path.isdir(path):
-            return [self._listdir(path)]
-        # exact file match terminates this completion
-        return os.path.abspath(path)
-
-
-def check_tmpl(var, default=None, tab_complete=False):
-    """Get entries for template."""
-    if tab_complete:
-        comp = Completer()
-        readline.set_completer(comp.complete)
-        readline.set_completer_delims('')
-        readline.parse_and_bind("tab: complete")
-    if default is None:
-        inpt = input('Enter {}:'.format(var))
-    else:
-        inpt = input('Enter {} [default {}]: '.format(var, default))
-    readline.set_completer(None)
-    if not inpt:
-        return default
-    else:
-        return inpt
-
-
-def create_nbkernel():
-    """Create a new karabo_data jupyter kernel if non exists."""
-    log.warn("A new kernel called 'xfel' will be created in your home directory")
-    inpt = input('Do you whish to continue [YES|no]:')
-    if 'no' in inpt.lower():
-        log.info('Aborting')
-        return
-    log.info('Creating xfel notebook kernel in userspace')
-    kernelspec.install(kernel_name='xfel', user=True)
-    log.info(
-        'The kernel can be loaded from the jupyter notbook menu: kernels -> change kernel -> xfel')
-
+NB_DIR = os.path.join(os.environ['HOME'], 'notebooks')
+NB_FILE = 'GeoAssembler.ipynb'
+CLEN = 0.119 #Default sample distance
+ENERGY = 10235 #Default beam energy
+# Default run directory
+RUNDIR = '/gpfs/exfel/exp/XMPL/201750/p700000/proc/r0005'
 
 def copy_notebook(defaults):
     """Create a new notebook and copy it into the user-space."""
-    parent = os.path.dirname(os.path.dirname(__file__))
+    parent = os.path.dirname(__file__)
     tmpl = os.path.join(parent, 'templates', 'geoAssembler.tmpl')
     try:
         os.makedirs(defaults['folder'])
@@ -137,49 +65,28 @@ def copy_notebook(defaults):
 
 
 def create_nb(rundir=None, geofile=None, clen=None, energy=None, levels=None,
-              nb_dir=None, nb_file=None, no_kernel=False):
+              nb_dir=None, nb_file=None):
     """Creat a notebook and copy it into the user space."""
     # Get the default varaibles for the notebook, if user has already given them
     # take them if not ask for them and provide default options
     nb_defaults = {}
-    nb_defaults['rundir'] = repr(rundir or check_tmpl(
-        'Run Directory',
-        default='/gpfs/exfel/exp/XMPL/201750/p700000/proc/r0005',
-        tab_complete=True))
-    geofile = geofile or check_tmpl('Input Geometry File in CFEL format',
-                                    default=None,
-                                    tab_complete=True)
+    nb_defaults['rundir'] = repr(rundir or RUNDIR)
     if geofile is not None:
         nb_defaults['geofile'] = repr(geofile)
     else:
         nb_defaults['geofile'] = None
-    levels = levels or check_tmpl(
-        'Min/Max Display Limits', default=None)
-    if isinstance(levels, str):
-        for repl in ('/', ',', '|', ';', ':'):
-            levels = levels.replace(repl, ' ')
-        levels = [float(lev) for lev in levels.strip().split()]
-    elif levels is None:
+    if levels is None:
         levels = [None, None]
     else:
         levels = levels
     nb_defaults['levels'] = levels
-    nb_defaults['clen'] = clen or check_tmpl('Detector Distance [m]',
-                                             default=str(clen))
-    nb_defaults['energy'] = energy or check_tmpl('Beam Wavelength [eV]',
-                                                 default=str(energy))
-    nb_defaults['folder'] = nb_dir or check_tmpl('Directory where your notebooks should be located',
-                                                 tab_complete=True,
-                                                 default=os.path.join(os.environ['HOME'],
-                                                                      'notebooks'))
-    name = nb_file or check_tmpl('Name of the geoAssembler notebook',
-                                 tab_complete=True,
-                                 default='GeoAssembler.ipynb')
+    nb_defaults['energy'] = energy
+    nb_defaults['folder'] = nb_dir or NB_DIR
+    name = nb_file or NB_FILE
     if not name.endswith('.ipynb'):
         name += '.ipynb'
     nb_defaults['name'] = os.path.basename(name)
-    if not no_kernel:
-        create_nbkernel()
+    nb_defaults['clen'] = clen or CLEN
     copy_notebook(nb_defaults)
 
 
@@ -206,31 +113,34 @@ def main(argv=None):
     ap.add_argument('-nb', '--notebook', default=False, action='store_true',
                     help='Create a notebook from a template that is saved in '
                          'your userspace')
-    ap.add_argument('-no_kernel', default=False, action='store_true',
-                    help='Do not try to attempt creating a xfel notebook kernel')
-    ap.add_argument('-nb_folder', default=None,
+    ap.add_argument('-nb_dir',
+                    default=NB_DIR,
                     help='Set default directory to save notebooks')
-    ap.add_argument('-nb_file', default=None,
-                    help='Set file name of the notbook')
-    ap.add_argument('-r', '--run', default=None,
-                    help='Select a run')
+    ap.add_argument('-nb_file', default=NB_FILE,
+                    help='Set file name of the notbook (default {})'.format(NB_FILE))
+    ap.add_argument('-r', '--rundir', default=RUNDIR,
+                    help='Select a run (default {})'.format(RUNDIR))
     ap.add_argument('-g', '--geometry', default=None,
-                    help='Select a cfel geometry file')
-    ap.add_argument('-c', '--clen', default=0.119,
-                    help='Detector distance [m]')
-    ap.add_argument('-e', '--energy', default=10235,
-                    help='Photon energy [ev]')
-    ap.add_argument('-l', '--level', nargs=2, default=None,
+                    help='Select a cfel geometry file (default None)')
+    ap.add_argument('-c', '--clen', default=CLEN,
+                    help='Detector distance [m] (default {})'.format(CLEN))
+    ap.add_argument('-e', '--energy', default=ENERGY,
+                    help='Photon energy [ev] (default {})'.format(ENERGY))
+    ap.add_argument('-l', '--level', nargs=2, default=[None, None],
                     help='Pre defined display range for plotting')
+    ap.add_argument('-t', '--test', default=False, action='store_true',
+                    help='Test mode')
 
     args = ap.parse_args()
 
     if args.notebook:
-        create_nb(args.run, args.geometry, args.clen, args.energy, args.level,
-                  args.nb_folder, args.nb_file, args.no_kernel)
+        create_nb(args.rundir, args.geometry, args.clen, args.energy, args.level,
+                  args.nb_dir, args.nb_file)
     else:
         create_calibrate_gui(args.run, args.geometry, levels=args.level,
-                             header=HEADER.format(clen=args.clen, energy=args.energy))
+                             header=HEADER.format(clen=args.clen,
+                                                  energy=args.energy),
+                             test=args.test)
 
 
 if __name__ == '__main__':
