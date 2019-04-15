3949394b83d55259b23f490a9dc4190307aea92e
Martin Bergemann <martin.bergemann@xfel.eu>
2019-04-15 10:58:46 +0200

    Delete module scope for calib

--- geoAssembler/tests/conftest.py
+++ geoAssembler/tests/conftest.py
@@ -1,22 +1,24 @@
 import pytest
-from tempfile import TemporaryDirectory
-import sys
 
-from pyqtgraph import QtGui
 
-from .utils import create_test_directory
-from ..qt_viewer import CalibrateQt
-
-APP = QtGui.QApplication(sys.argv)
+@pytest.fixture(scope='session')
+def gui_app():
+    import sys
+    from pyqtgraph import QtGui
+    app = QtGui.QApplication(sys.argv)
+    yield app
 
 @pytest.fixture(scope='module')
 def mock_run():
     """Create a test run with predev ring data."""
+    from .utils import create_test_directory
+    from tempfile import TemporaryDirectory
+
     with TemporaryDirectory() as td:
          create_test_directory(td)
          yield td
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def calib():
     """Create the calibration gui"""
     # Define a header for the cfel geometry file
@@ -26,5 +28,6 @@ def calib():
     adu_per_eV = 0.0075
     photon_energy = 10235;
     """
+    from ..qt_viewer import CalibrateQt
 
     yield CalibrateQt(None, None, header=header)
