981d3d8783b616c85c238852abed2951b500e9a9
Martin Bergemann <martin.bergemann@xfel.eu>
2019-04-25 15:47:10 +0200

    Add flag for test data type

--- geoAssembler/main.py
+++ geoAssembler/main.py
@@ -110,26 +110,28 @@ def main(argv=None):
 
     To select quadrants click on the quadrant and to move the selected quadrant
     use CTRL+arrow-keys.""")
-    ap.add_argument('-nb', '--notebook', default=False, action='store_true',
+    ap.add_argument('--notebook', default=False, action='store_true',
                     help='Create a notebook from a template that is saved in '
                          'your userspace')
-    ap.add_argument('-nb_dir',
+    ap.add_argument('--nb_dir',
                     default=NB_DIR,
                     help='Set default directory to save notebooks')
-    ap.add_argument('-nb_file', default=NB_FILE,
+    ap.add_argument('--nb_file', default=NB_FILE,
                     help='Set file name of the notbook (default {})'.format(NB_FILE))
-    ap.add_argument('-r', '--rundir', default=None,
+    ap.add_argument('--rundir', default=None,
                     help='Select a run (default {})'.format(RUNDIR))
-    ap.add_argument('-g', '--geometry', default=None,
+    ap.add_argument('--geometry', default=None,
                     help='Select a cfel geometry file (default None)')
-    ap.add_argument('-c', '--clen', default=CLEN,
+    ap.add_argument('--clen', default=CLEN,
                     help='Detector distance [m] (default {})'.format(CLEN))
-    ap.add_argument('-e', '--energy', default=ENERGY,
+    ap.add_argument('--energy', default=ENERGY,
                     help='Photon energy [ev] (default {})'.format(ENERGY))
-    ap.add_argument('-l', '--level', nargs=2, default=[None, None],
+    ap.add_argument('--level', nargs=2, default=[None, None], type=float,
                     help='Pre defined display range for plotting')
-    ap.add_argument('-t', '--test', default=False, action='store_true',
+    ap.add_argument('--test', default=False, action='store_true',
                     help='Test mode')
+    ap.add_argument('--det', default='AGIPD', choices=('AGIPD', 'LPD'),
+                    help='If test mode is activated, the name of the detector')
 
     args = ap.parse_args()
 
@@ -142,7 +144,7 @@ def main(argv=None):
             from .tests.utils import create_test_directory
             with TemporaryDirectory() as td:
                 log.info('Creating temp data in {}...'.format(td))
-                create_test_directory(td)
+                create_test_directory(td, det=args.det)
                 log.info('...done')
                 create_calibrate_gui(td,
                                      args.geometry,
