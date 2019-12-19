9ad82a8f9ee5270d956f79786b238975d658dff8
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2019-12-19 14:07:42 +0100

    Add upper bound to pyqt versions

--- setup.py
+++ setup.py
@@ -58,8 +58,8 @@ setup(name="geoAssembler",
           'pyqtgraph',
           'ipywidgets',
           'pyFai',
-          'PyQt5 >= 5.13.2',
-          'PyQt5-sip >= 12.7.0',
+          'PyQt5 >= 5.13.2, <= 6.0.0',
+          'PyQt5-sip >= 12.7.0, <= 13.0.0',
           'scipy'
 
       ],
