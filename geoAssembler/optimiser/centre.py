8de60ddbfe51acc16b6bacd449213999d4e5b082
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-05-08 14:23:48 +0200

    Update docstrings

--- geoAssembler/optimiser/centre.py
+++ geoAssembler/optimiser/centre.py
@@ -47,7 +47,7 @@ class CentreOptimiser:
         ----------
 
         centre_offset: pair of values
-            Sets the centre position of the search grid.
+            Sets the offset form the current centre value to test for.
         """
         res = self.integrator.integrate2d(
             self.frame,
@@ -59,27 +59,19 @@ class CentreOptimiser:
 
     def optimise(self, bounds=[(-50, 50), (-50, 50)], workers=1, verbose=False):
         """
-        Function which performs a grid-search which goes from coarse to fine
-        coordinates, defined by `r_step_pairs`.
+        Find the optimal centre position via Scipy's `differential_evolution`
+        global optimiser.
 
         Parameters
         ----------
 
-        r_step_pairs: list of tuples
-            A list of tuple pair of radius and step size, where the optimum
-            coordinates of the previous grid are used as the centre for the
-            subsequent grid.
+        bounds: list of tuples
+            set the search area for the optimiser
 
-            e.g. `[(50, 10), (10, 2)]` creates a grid of pixel of pixel
-            coordinates from -50 to +50, with steps of 10, so an 11x11 grid.
+        workers: int
+            set the number of workers (cores) to use, -1 for auto
 
-            The optimal position is then fed into the next step, which uses a
-            grid from -10 to +10, with steps of 2, centred on the previous pos.
-
-        pool: a multiprocessing Pool object
-            Used to enable multiprocessing over multiple threads, it is
-            recommended to only use 32 threads, the number of threads
-            should increase if larger grids are used.
+        verbose: bool
         """
         res_tuple = namedtuple(
             "OptimiseResult", "optimal_quad_positions optimal_offset results"
