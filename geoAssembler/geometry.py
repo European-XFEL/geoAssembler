7359c56cd6a95e89fa070dfae6b091885f5ccec4
Thomas Kluyver <thomas@kluyver.me.uk>
2020-08-24 16:03:22 +0100

    Try to fix getting quadrant positions for DSSC

--- geoAssembler/geometry.py
+++ geoAssembler/geometry.py
@@ -317,8 +317,7 @@ class DSSCGeometry(GeometryAssembler):
             mod_grp = f['Q{}/M{}'.format(quad, module)]
             mod_offset = mod_grp['Position'][:]
             tile_offset = mod_grp['T{:02}/Position'.format(asic)][:]
-            cr_pos *= self._px_conv
-        return cr_pos - (mod_offset + tile_offset)
+        return (cr_pos * self.unit) - (mod_offset + tile_offset)
 
 
 
