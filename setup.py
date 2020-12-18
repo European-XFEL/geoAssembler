8c5c8b06db16a129e00c6727e3d53b849e18acb4
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-12-18 11:38:22 +0100

    Indent to 4 spaces

--- setup.py
+++ setup.py
@@ -29,62 +29,63 @@ def find_version(*parts):
 def find_files(directory):
     return glob.glob(osp.join(directory, '*'))
 
-setup(name="geoAssembler",
-      version=find_version("geoAssembler", "__init__.py"),
-      author="European XFEL GmbH",
-      author_email="cas-support@xfel.eu",
-      maintainer="Martin Bergemann",
-      url="https://git.xfel.eu/gitlab/dataAnalysis/geoAssembler",
-      description="Tool to Calibrate Detector Geometry Based on Power Diffraction Pattern",
-      long_description=read("README.md"),
-      long_description_content_type='text/markdown',
-      license="BSD-3-Clause",
-      packages=find_packages(),
-      data_files=[('templates', find_files('templates')),
-                  ('cells', find_files('cells'))],
-      package_data={'': ['cells/*.D', 'templates/*.tmpl', 'icons/*.png'],
-                    'geoAssembler.tests': ['data_*.npz', 'test.geom'],
-                    'geoAssembler.qt.editor': ['*.ui'],
-                    },
-      entry_points={
-          'gui_scripts': [
-              'geoAssembler = geoAssembler.main:main'
-          ]},
-      install_requires=[
-          'cfelpyutils',
-          'EXtra-data',
-          'EXtra-geom >=1.1.0',
-          'matplotlib',
-          'numpy',
-          'pyqtgraph',
-          'ipywidgets',
-          'pyFai',
-          'PyQt5 >= 5.13.2, <= 6.0.0',
-          'PyQt5-sip >= 12.7.0, <= 13.0.0',
-          'scipy',
-          'xarray >= 0.14.1',
-      ],
-      extras_require={
-          'docs': [
-              'sphinx',
-              'nbsphinx',
-              'ipython',  # For nbsphinx syntax highlighting
-          ],
-          'test': [
-              'pytest',
-              'testpath',
-          ]
-      },
-      python_requires='>=3.4',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3',
-          'Topic :: Scientific/Engineering :: Information Analysis',
-          'Topic :: Scientific/Engineering :: Physics',
-      ]
-      )
+setup(
+    name="geoAssembler",
+    version=find_version("geoAssembler", "__init__.py"),
+    author="European XFEL GmbH",
+    author_email="cas-support@xfel.eu",
+    maintainer="Martin Bergemann",
+    url="https://git.xfel.eu/gitlab/dataAnalysis/geoAssembler",
+    description="Tool to Calibrate Detector Geometry Based on Power Diffraction Pattern",
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    data_files=[('templates', find_files('templates')),
+                ('cells', find_files('cells'))],
+    package_data={'': ['cells/*.D', 'templates/*.tmpl', 'icons/*.png'],
+                'geoAssembler.tests': ['data_*.npz', 'test.geom'],
+                'geoAssembler.qt.editor': ['*.ui'],
+                },
+    entry_points={
+        'gui_scripts': [
+            'geoAssembler = geoAssembler.main:main'
+        ]},
+    install_requires=[
+        'cfelpyutils',
+        'EXtra-data',
+        'EXtra-geom >=1.1.0',
+        'matplotlib',
+        'numpy',
+        'pyqtgraph',
+        'ipywidgets',
+        'pyFai',
+        'PyQt5 >= 5.13.2, <= 6.0.0',
+        'PyQt5-sip >= 12.7.0, <= 13.0.0',
+        'scipy',
+        'xarray >= 0.14.1',
+    ],
+    extras_require={
+        'docs': [
+            'sphinx',
+            'nbsphinx',
+            'ipython',  # For nbsphinx syntax highlighting
+        ],
+        'test': [
+            'pytest',
+            'testpath',
+        ]
+    },
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Physics',
+    ]
+)
